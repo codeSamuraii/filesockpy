fa130434098b2f68de5ad05fa834f9f2cb1bd5f6
codesamuraii <remi@heneault.fr>
2023-08-02 20:49:53 +0200

    Simplification

--- api/io.py
+++ api/io.py
@@ -1,5 +1,4 @@
 import eventlet
-eventlet.monkey_patch()
 import socket
 import logging
 import pathlib
@@ -10,75 +9,57 @@ log.setLevel(logging.DEBUG)
 
 
 class FileQueue:
-    def __init__(self, file_path: pathlib.Path, buff_size: int = 1024, n_jobs: int = 2):
+    def __init__(self, file_path: pathlib.Path, buff_size: int = 1024, n_jobs: int = 4):
         self.input = file_path
         self.n_jobs = n_jobs
         self.buff_size = buff_size
-        self.pool = eventlet.GreenPool(n_jobs + 1)
-        self.read_pile = eventlet.GreenPile(self.pool)
-        self.write_pile = eventlet.GreenPile(self.pool)
+        self.pool = eventlet.GreenPool()
         self.queue = eventlet.Queue()
 
-    def read_file_to_queue(self, alt_input_io: io.FileIO = None):
-        input_io = alt_input_io or self.input.open('rb', buffering=0)
-        try:
-            self._file_to_queue(input_io, self.queue, self.buff_size, self.n_jobs)
-        finally:
-            input_io.close()
-
     @staticmethod
-    def _file_to_queue(input_io: io.FileIO, queue: eventlet.Queue, buff_size: int, n_jobs: int):
+    def _read_and_queue(input_io: io.FileIO, output_queue: eventlet.Queue, buff_size: int):
         while True:
-            bytes_read = input_io.read(buff_size * n_jobs)
-    
+            bytes_read = input_io.read(buff_size)
             if not bytes_read:
-                queue.put_nowait('\0')
-                log.debug("> sending EOF")
                 break
-
-            log.debug(f'> {len(bytes_read)} bytes read from file')
-            chunks = (bytes_read[i:i+buff_size] for i in range(0, len(bytes_read), buff_size))
-            for chunk in chunks:
-                queue.put_nowait(chunk)
+            else:
+                output_queue.put_nowait(bytes_read)
 
             eventlet.sleep()
     
-    @classmethod
-    def read(cls, file_path: pathlib.Path, buff_size: int = 1024, n_jobs: int = 2):
-        file_queue = cls(file_path, buff_size, n_jobs)
-        file_queue.read_pile.spawn(file_queue.read_file_to_queue)
-        return file_queue
-
-    @staticmethod
-    def _read_queue(queue: eventlet.Queue, callback: callable):
+    def read_and_queue(self):
+        input_io = self.input.open('rb', buffering=self.buff_size)
         try:
-            while queue_item := queue.get(timeout=5.0):
-                if queue_item == '\0':
-                    log.debug('< end of queue')
-                    queue.put_nowait('\0')
-                    break
-
-                log.debug(f'< {len(queue_item)} bytes received')
-                callback(queue_item)
-                eventlet.sleep()
-    
-        except eventlet.queue.Empty:
-            log.warn('< queue empty for 5 seconds !')
-
-        eventlet.sleep()
+            jobs = {
+                self.pool.spawn(self._read_and_queue, input_io, self.queue, self.buff_size) for _ in range(self.n_jobs)
+            }
+            self.pool.waitall()
+        finally:
+            input_io.close()
     
     @staticmethod
-    def _log_queue(queue: eventlet.Queue):
-        FileQueue._read_queue(queue, log.info)
+    def _read_and_send(input_io: io.FileIO, output_socket: socket.socket, buff_size: int):
+        while True:
+            bytes_read = input_io.read(buff_size)
+            if not bytes_read:
+                break
+            else:
+                output_socket.sendall(bytes_read)
+                logging.debug(f"sent {len(bytes_read)} bytes")
 
-    # def queue_to_file(self):
-    #     while queue_item := self.queue.get():
-    #         print(queue_item)   
- 
-    # def write(cls, file_path: pathlib.Path, buff_size: int = 256, n_jobs: int = 2):
-    #     object = cls(file_path, buff_size, n_jobs)
-    #     object.pool.spawn(object.queue_to_file)
-    #     return object, object.queue
+            eventlet.sleep()
+    
+    def read_and_send(self, output_socket: socket.socket):
+        input_io = self.input.open('rb', buffering=self.buff_size)
+        try:
+            jobs = {
+                self.pool.spawn(self._read_and_send, input_io, output_socket, self.buff_size) for _ in range(self.n_jobs)
+            }
+            self.pool.waitall()
+        finally:
+            input_io.close()
+
+
 
 class FileSend(FileQueue):
     def __init__(self, file_path: pathlib.Path, host: str, port: int, buff_size: int = 1024, n_jobs: int = 2):
@@ -100,6 +81,6 @@ if __name__ == '__main__':
     s = socket.socket()
     s.connect(('127.0.0.1', 9898))
 
-    fq = FileQueue.read(input_path, buff_size=32, n_jobs=n_jobs)
-    writers = [fq.write_pile.spawn(FileSend._send_queue, fq.queue, s) for _ in range(n_jobs)]
+    fq = FileQueue(input_path, n_jobs=n_jobs)
+    fq.read_and_send(s)
     fq.pool.waitall()
